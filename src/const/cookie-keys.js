093468f45bab35da10a997d4e32992f0237b5ec2
test <test@qq.com>
2020-05-21 18:49:49 +0800

    feat: 可视化代码推送

--- src/const/cookie-keys.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by levy on 2019/1/24.
- */
-
-export default [
-  '_token',
-  'iamAppId'
-]
--- src/plugins/globalPlugin.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import openAxios from '@/utils/openApiFetch'
-export default function( ) {
-  Vue.prototype.$openAxios = openAxios
-}
